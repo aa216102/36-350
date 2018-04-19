70bcd2322067ee3d609f5fa7adb4fbcfe9157c70
aa216102 <38470472+aa216102@users.noreply.github.com>
2018-04-19 12:53:09 -0400

    Revised model_select()

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -5,9 +5,9 @@ generate_data = function(n,p){
   return(list(covariates = covariates, responses = responses))
 }
 
-
-
-
-
-
-
+model_select = function(covariates, responses, cutoff){
+  regression = lm(responses~covariates)
+  cutoff.indices = summary(regression)$coefficients[2:length(summary(regression)$coefficients[,4]),4]<=cutoff
+  cutoff.regression = lm(responses ~ covariates[,cutoff.indices])
+  return(cutoff.regression)
+}
