09473782c25cdefd3778ea8afd60ae30697edf36
aa216102 <38470472+aa216102@users.noreply.github.com>
2018-04-19 12:50:37 -0400

    Revised generate_data()

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -1,13 +1,13 @@
 # 36-350
 generate_data = function(n,p){
-  covariates = matrix(rnorm(n*p,0,1))
+  covariates = matrix(rnorm(n*p,0,1), nrow = n, ncol = p)
   responses = rnorm(n,0,1)
   return(list(covariates = covariates, responses = responses))
 }
 
-model_select = function(covariates, responses, cutoff){
-  regression = lm(responses~covariates)
-  cutoff.regression = lm(responses ~ 
-                           covariates[,regression$coefficients[,4]<=cutoff])
-  return(cutoff.regression)
-}
+
+
+
+
+
+
