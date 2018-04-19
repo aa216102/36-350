b60c4d89a9fcc9d831a80af569d5118a0cb1d373
aa216102 <38470472+aa216102@users.noreply.github.com>
2018-04-19 11:31:42 -0400

    Part 2b, shows model_select()

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -4,3 +4,10 @@ generate_data = function(n,p){
   responses = rnorm(n,0,1)
   return(list(covariates = covariates, responses = responses))
 }
+
+model_select = function(covariates, responses, cutoff){
+  regression = lm(responses~covariates)
+  cutoff.regression = lm(responses ~ 
+                           covariates[,regression$coefficients[,4]<=cutoff])
+  return(cutoff.regression)
+}
